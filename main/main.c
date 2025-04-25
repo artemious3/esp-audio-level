59f88191cd1eeb81584c097e785de5d640ff6c79
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-04-25 12:37:12 +0300

    Replace custom rror handling with ESP-IDF provided

--- main/main.c
+++ main/main.c
@@ -14,6 +14,10 @@
 #include "bt.h"
 
 #include "driver/gpio.h"
+#include "driver/i2c_master.h"
+#include "esp_lcd_io_i2c.h"
+
+#include "sdkconfig.h"
 
 #define RETURN_ERR(str)                                 \
   if (err != ESP_OK) {                                  \
@@ -22,72 +26,71 @@
     return err;                                         \
   }
 
-static esp_err_t gpio_init() {
-  esp_err_t err;
+static void gpio_init() {
 
-  err = gpio_reset_pin(CONFIG_GPIO_MUSIC_LED);
-  RETURN_ERR("failed to reset gpio pin");
-  err = gpio_reset_pin(CONFIG_GPIO_MUSIC_LED);
-  RETURN_ERR("failed to reset gpio pin");
+  ESP_ERROR_CHECK(gpio_reset_pin(CONFIG_GPIO_MUSIC_LED));
+  ESP_ERROR_CHECK(gpio_reset_pin(CONFIG_GPIO_MUSIC_LED));
 
-  err = gpio_set_direction(CONFIG_GPIO_BT_STATE_LED, GPIO_MODE_OUTPUT);
-  RETURN_ERR("failed to set gpio direction");
-  err = gpio_set_direction(CONFIG_GPIO_MUSIC_LED, GPIO_MODE_OUTPUT);
-  RETURN_ERR("failed to set gpio direction");
+  ESP_ERROR_CHECK(
+      gpio_set_direction(CONFIG_GPIO_BT_STATE_LED, GPIO_MODE_OUTPUT));
+  ESP_ERROR_CHECK(gpio_set_direction(CONFIG_GPIO_MUSIC_LED, GPIO_MODE_OUTPUT));
 
-  return 0;
 }
 
+static void bt_init() {
 
-static esp_err_t bt_init() {
-
-  esp_err_t err;
-
-  err = nvs_flash_init();
-  RETURN_ERR("could not init nvs")
+  ESP_ERROR_CHECK(nvs_flash_init());
 
   esp_bt_controller_config_t cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-  err = esp_bt_controller_init(&cfg);
-  RETURN_ERR("could not init bluetooth controller")
+  ESP_ERROR_CHECK(esp_bt_controller_init(&cfg));
 
-  err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT);
-  RETURN_ERR("could not enable bluetooth controller")
+  ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT));
 
-  err = esp_bluedroid_init();
-  RETURN_ERR("could not init bluedroid")
+  ESP_ERROR_CHECK(esp_bluedroid_init());
 
-  err = esp_bluedroid_enable();
-  RETURN_ERR("could not enable bluedroid")
+  ESP_ERROR_CHECK(esp_bluedroid_enable());
 
-  err = esp_bt_gap_set_device_name("ESP-EQ");
-  RETURN_ERR("could not set device name")
+  ESP_ERROR_CHECK(esp_bt_gap_set_device_name("ESP-EQ"));
 
-  err = esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
-  RETURN_ERR("could not make device connectable and discoverable")
+  ESP_ERROR_CHECK(esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE,
+                                           ESP_BT_GENERAL_DISCOVERABLE));
 
-  err = esp_a2d_sink_init();
-  RETURN_ERR("could not init a2d sink")
+  ESP_ERROR_CHECK(esp_a2d_sink_init());
 
-  err = esp_a2d_register_callback(bt_a2dp_cb);
-  RETURN_ERR("could not register callack")
+  ESP_ERROR_CHECK(esp_a2d_register_callback(bt_a2dp_cb));
 
-  err = esp_a2d_sink_register_data_callback(bt_data_cb);
-  RETURN_ERR("could not data register callack")
+  ESP_ERROR_CHECK(esp_a2d_sink_register_data_callback(bt_data_cb));
 
-  return ESP_OK;
 }
 
+static void lcd_init() {
+  esp_err_t err;
+
+  i2c_master_bus_handle_t i2c_bus = NULL;
+  i2c_master_bus_config_t bus_cfg = {
+      .clk_source = I2C_CLK_SRC_DEFAULT,
+      .glitch_ignore_cnt = 7,
+      .i2c_port = 0,
+      .sda_io_num = CONFIG_GPIO_I2C_SDA,
+      .scl_io_num = CONFIG_GPIO_I2C_SCL,
+      .flags.enable_internal_pullup = true,
+  };
+  ESP_ERROR_CHECK(i2c_new_master_bus(&bus_cfg, &i2c_bus));
+
+  esp_lcd_panel_io_handle_t io_handle = NULL;
+  esp_lcd_panel_io_i2c_config_t io_config = {.dev_addr = 0x3C,
+                                             .scl_speed_hz = (400 * 1000),
+                                             .control_phase_bytes = 1,
+                                             .lcd_cmd_bits = 8,
+                                             .lcd_param_bits = 8,
+                                             .dc_bit_offset = 6};
+  ESP_ERROR_CHECK(i2c_new_master_bus(&bus_cfg, &i2c_bus));
+}
 
 void app_main(void) {
 
-  esp_err_t err;
-  err = gpio_init();
-  if (err)
-	  abort();
-
-  err = bt_init();
-  if (err)
-	  abort();
+  gpio_init();
+  bt_init();
 
   printf("Bluetooth enabled!!! (^_^)");
 
